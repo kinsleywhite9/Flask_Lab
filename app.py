06f5865254ab13124d8baf55db51aa119eade888
kinsleywhite9 <kinsleywhite9@gmail.com>
2021-09-19 18:58:40 +0000

    testing 3

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
-if __name__ == "__main__":
-    app.run(debug = True)
 
 @app.route("/about")
 def about():
     name = request.args.get('name') if request.args.get('name') else "Hello World!" 
     return render_template("about.html", aboutName=name) 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
+if __name__ == "__main__":
+    app.run(debug = True)
